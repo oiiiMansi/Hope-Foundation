aa276c72ccec211908ef60f5be89c3f49b5ee55e
oiiiMansi <sainimansi360@gmail.com>
2025-08-03 12:30:03 +0530

    minor change

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/Hope-Foundation/', // 👈 IMPORTANT: Must match your repo name exactly
   plugins: [react()],
 })
